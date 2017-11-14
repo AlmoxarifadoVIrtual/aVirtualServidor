687321c9518bfbf68d1b845f5aa59f64ed8698ea
alessandrofook <alessandrofook@users.sourceforge.net>
2017-11-14 16:06:12 -0300

    Implementando padrão de resposta do backend.

--- src/main/java/lesufcg/almoxarifadovirtual/AvirtualFachada.java
+++ src/main/java/lesufcg/almoxarifadovirtual/AvirtualFachada.java
@@ -4,6 +4,8 @@ import lesufcg.almoxarifadovirtual.controle.Controle;
 import lesufcg.almoxarifadovirtual.modelo.usuario.FuncaoUsuario;
 import lesufcg.almoxarifadovirtual.modelo.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,49 +16,55 @@ public class AvirtualFachada {
     @Autowired
     private Controle controle;
 
-    @RequestMapping(value="/usuario", method = RequestMethod.POST)
+    //Métodos de Usuários
+    @RequestMapping(value = "/usuarios", method = RequestMethod.POST)
     @ResponseBody
-    public Usuario create(@RequestBody Usuario usuario) {
-        return controle.criarUsuario(usuario);
+    public ResponseEntity<Usuario> create(@RequestBody Usuario usuario) {
+        try {
+            return ResponseEntity.ok(controle.criarUsuario(usuario));
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
     }
 
-    @RequestMapping(value="/usuario", method = RequestMethod.GET)
+    @RequestMapping(value = "/usuarios/{id}", method = RequestMethod.GET)
     @ResponseBody
-    public Usuario getUsuario(@RequestHeader Long id) {
-        return controle.getUsuario(id);
+    public ResponseEntity<Usuario> getUsuario(@PathVariable Long id) {
+        return ResponseEntity.ok(controle.getUsuario(id));
     }
 
-    @RequestMapping(value="/usuario/listar", method = RequestMethod.GET)
+    @RequestMapping(value = "/usuarios", method = RequestMethod.GET)
     @ResponseBody
     public List<Usuario> getAll() {
         return controle.getAllUsuarios();
     }
 
-    @RequestMapping(value="/usuario", method = RequestMethod.PUT)
+    @RequestMapping(value = "/usuarios/{id}", method = RequestMethod.PUT)
     @ResponseBody
-    public boolean update(@RequestBody Usuario usuario) {
+    public boolean update(@PathVariable Usuario usuario) {
         return controle.atualizarUsuario(usuario);
     }
 
-    @RequestMapping(value="/usuario", method = RequestMethod.DELETE)
+    @RequestMapping(value = "/usuarios/{id}", method = RequestMethod.DELETE)
     @ResponseBody
-    public boolean delete(@RequestHeader Long id) {
-        return controle.deletarUsuario(id);
+    public void delete(@PathVariable Long id) {
+        controle.deletarUsuario(id);
     }
 
-    @RequestMapping(value="/usuario/listar/administradores", method = RequestMethod.GET)
+    @RequestMapping(value = "/usuarios/administradores", method = RequestMethod.GET)
     @ResponseBody
     public List<Usuario> listarAdministradores() {
         return controle.getUsuarioByFuncao(FuncaoUsuario.ADMINISTRADOR);
     }
 
-    @RequestMapping(value="/usuario/listar/almoxarifes", method = RequestMethod.GET)
+    @RequestMapping(value = "/usuario/almoxarifes", method = RequestMethod.GET)
     @ResponseBody
     public List<Usuario> listarAlmoxarifes() {
         return controle.getUsuarioByFuncao(FuncaoUsuario.ALMOXARIFE);
     }
 
-    @RequestMapping(value="/usuario/listar/prestadores", method = RequestMethod.GET)
+    @RequestMapping(value = "/usuario/prestadores", method = RequestMethod.GET)
     @ResponseBody
     public List<Usuario> listarPrestadores() {
         return controle.getUsuarioByFuncao(FuncaoUsuario.PRESTADOR);
