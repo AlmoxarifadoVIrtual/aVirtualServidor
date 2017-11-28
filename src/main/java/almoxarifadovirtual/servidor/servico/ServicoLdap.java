aecf816d2a510f68aa372a90cbb2bedceb922c13
Matteus Silva <silvamatteus@lsd.ufcg.edu.br>
2017-11-27 21:30:03 -0300

    Formatando segundo google checkstyle

--- src/main/java/almoxarifadovirtual/servidor/servico/ServicoLdap.java
+++ src/main/java/almoxarifadovirtual/servidor/servico/ServicoLdap.java
@@ -9,26 +9,26 @@ import java.util.List;
 @Service
 public class ServicoLdap {
 
-    private List<String[]> usuariosLdap;
+  private List<String[]> usuariosLdap;
 
-    public ServicoLdap() {
-        this.usuariosLdap = new ArrayList<String[]>();
-        this.usuariosLdap.add(new String[]{"Matteus", "passwd-admin"});
-        this.usuariosLdap.add(new String[]{"Alessandro", "passwd-almoxarife"});
-        this.usuariosLdap.add(new String[]{"Lucas", "passwd-almoxarife"});
-        this.usuariosLdap.add(new String[]{"Bernard", "passwd-prestador"});
-        this.usuariosLdap.add(new String[]{"Rafael", "passwd-prestador"});
+  public ServicoLdap() {
+    this.usuariosLdap = new ArrayList<String[]>();
+    this.usuariosLdap.add(new String[] {"Matteus", "passwd-admin"});
+    this.usuariosLdap.add(new String[] {"Alessandro", "passwd-almoxarife"});
+    this.usuariosLdap.add(new String[] {"Lucas", "passwd-almoxarife"});
+    this.usuariosLdap.add(new String[] {"Bernard", "passwd-prestador"});
+    this.usuariosLdap.add(new String[] {"Rafael", "passwd-prestador"});
+  }
+
+  public boolean ehUsuarioLdap(Credenciais credenciais) {
+
+    boolean ehUsuarioValido = false;
+    for (String[] usuario : usuariosLdap) {
+      if (usuario[0].equals(credenciais.getLogin()) && usuario[1].equals(credenciais.getSenha())) {
+        ehUsuarioValido = true;
+        break;
+      }
     }
-
-    public boolean ehUsuarioLdap(Credenciais credenciais) {
-
-        boolean ehUsuarioValido = false;
-        for (String[] usuario : usuariosLdap) {
-            if (usuario[0].equals(credenciais.getLogin()) && usuario[1].equals(credenciais.getSenha())) {
-                ehUsuarioValido = true;
-                break;
-            }
-        }
-        return ehUsuarioValido;
-    }
+    return ehUsuarioValido;
+  }
 }
