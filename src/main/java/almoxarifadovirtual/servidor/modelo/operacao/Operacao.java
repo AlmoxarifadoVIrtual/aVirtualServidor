d3fbafed56cbfd51ec77461d368e2617cf7fe6e8
alessandroliafook <alessandro.liafook@gmail.com>
2017-11-30 22:49:28 -0300

    Criando a branch para desenvolvimento dos métodos relativos ao caso de uso 03, com a definição base das rotas.

---
+++ src/main/java/almoxarifadovirtual/servidor/modelo/operacao/Operacao.java
@@ -0,0 +1,10 @@
+package almoxarifadovirtual.servidor.modelo.operacao;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity(name = "Operacao")
+@Table(name = "tb_operacao")
+public class Operacao {
+
+}
---
+++ src/main/java/almoxarifadovirtual/servidor/modelo/produto/Produto.java
@@ -0,0 +1,10 @@
+package almoxarifadovirtual.servidor.modelo.produto;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity(name = "Produto")
+@Table(name = "tb_produto")
+public class Produto {
+
+}
