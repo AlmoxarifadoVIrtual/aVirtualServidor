3c7f2a9273125b33695d577285591923ff8a8680
alessandroliafook <alessandro.liafook@gmail.com>
2018-02-27 20:50:42 -0300

    Implementando cross de teste.

--- src/main/java/almoxarifadovirtual/servidor/controle/ControleDeAcesso.java
+++ src/main/java/almoxarifadovirtual/servidor/controle/ControleDeAcesso.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "https://xavierlr.github.io")
 @RestController
 @RequestMapping("/acesso")
 public class ControleDeAcesso {
